e9fe9a8ae51ed7f38504aabac5e268275fcb014b
Swaraj <swarajchavan2004@gmail.com>
2023-10-19 00:31:56 +0530

    updated news routes and controller

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -2,7 +2,7 @@ const NewsAPI = require("newsapi");
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 
 // @desc Get news
-// @route GET /news
+// @route POST /news
 // @access Public
 const getNews = (req, res) => {
     const { limit, page } = req.body;
--- server/routes/newsRoutes.js
+++ server/routes/newsRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const newsController = require("../controllers/newsController");
 
-router.route("/news").get(newsController.getNews);
+router.route("/news").post(newsController.getNews);
 
 module.exports = router;
