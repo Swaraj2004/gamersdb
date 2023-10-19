534dc756b4a20a3900f734c454341626a9a2b145
Swaraj <swarajchavan2004@gmail.com>
2023-10-19 14:20:24 +0530

    updated news controller

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -6,15 +6,23 @@ const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 // @access Public
 const getNews = (req, res) => {
     const { limit, page } = req.body;
+
+    if (!limit) {
+        return res.status(400).json({
+            message: "Page limit is required",
+            success: false,
+        });
+    }
+
     newsapi.v2
         .everything({
             domains: "eurogamer.net,kotaku.com",
             language: "en",
-            pageSize: limit || 30,
+            pageSize: limit,
             page: page || 1,
         })
         .then((response) => {
-            res.json({ result: response, success: true });
+            res.json({ result: response.articles, success: true });
         })
         .catch((error) => {
             res.status(500).json({ message: error.message, success: false });
