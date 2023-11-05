35cb3ce6dc289895fd10567f0a33b8110623ad2f
Swaraj <swarajchavan2004@gmail.com>
2023-11-05 22:29:02 +0530

    updated allowedOrigins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,3 @@
-const allowedOrigins = [
-    "http://localhost:3000",
-    "https://gamersdb.onrender.com",
-];
+const allowedOrigins = ["http://localhost:3000", "https://gamersdb.vercel.app"];
 
 module.exports = allowedOrigins;
