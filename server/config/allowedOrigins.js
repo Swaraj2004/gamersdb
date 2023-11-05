01409b1f444661999ff067ad9010624ec350f6ff
Swaraj <swarajchavan2004@gmail.com>
2023-11-06 00:34:57 +0530

    updated allowedOrigins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "https://gamersdb.vercel.app"];
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://gamersdb.vercel.app",
+    "https://gamersdb.netlify.app",
+];
 
 module.exports = allowedOrigins;
