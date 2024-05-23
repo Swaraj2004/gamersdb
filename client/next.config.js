4d931896b7fd3776a53debed3af7a41ae43efb9b
Swaraj Chavan <swarajchavan2004@gmail.com>
2024-05-23 14:43:24 +0530

    Update next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,25 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.igdb.com",
-            },
-            {
-                protocol: "https",
-                hostname: "assetsio.reedpopcdn.com",
-            },
-            {
-                protocol: "https",
-                hostname: "i.kinja-img.com",
-            },
-            {
-                protocol: "https",
-                hostname: "www.destructoid.com",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.igdb.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assetsio.reedpopcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assetsio.gnwcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.kinja-img.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.destructoid.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
